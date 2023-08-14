a89dade4739af6420d43c97ef67fbaed43f639b5
Rishita Maheshwari <140805496+rishita0205@users.noreply.github.com>
2023-08-14 23:10:49 +0530

    Add files via upload

---
+++ onthisdate/js/initialization.js
@@ -0,0 +1,175 @@
+			var month;
+			var determinedate = "/date";
+			function initmonth1() {
+				month = 1;
+				return month;
+			}
+			function initmonth2() {
+				month = 2;
+				return month;
+			}
+			function initmonth3() {
+				month = 3;
+				return month;
+			}
+			function initmonth4() {
+				month = 4;
+				return month;
+			}
+			function initmonth5() {
+				month = 5;
+				return month;
+			}
+			function initmonth6() {
+				month = 6;
+				return month;
+			}
+			function initmonth7() {
+				month = 7;
+				return month;
+			}
+			function initmonth8() {
+				month = 8;
+				return month;
+			}
+			function initmonth9() {
+				month = 9;
+				return month;
+			}
+			function initmonth10() {
+				month = 10;
+				return month;
+			}
+			function initmonth11() {
+				month = 11;
+				return month;
+			}
+			function initmonth12() {
+				month = 12;
+				return month;
+			}
+			
+			//----------------------------------------------------------------------------------//
+			
+			var day;
+			function initday1() {
+				day = 1;
+				return day;
+			}
+			function initday2() {
+				day = 2;
+				return day;
+			}
+			function initday3() {
+				day = 3;
+				return day;
+			}
+			function initday4() {
+				day = 4;
+				return day;
+			}
+			function initday5() {
+				day = 5;
+				return day;
+			}
+			function initday6() {
+				day = 6;
+				return day;
+			}
+			function initday7() {
+				day = 7;
+				return day;
+			}
+			function initday8() {
+				day = 8;
+				return day;
+			}
+			function initday9() {
+				day = 9;
+				return day;
+			}
+			function initday10() {
+				day = 10;
+				return day;
+			}
+			function initday11() {
+				day = 11;
+				return day;
+			}
+			function initday12() {
+				day = 12;
+				return day;
+			}
+			function initday13() {
+				day = 13;
+				return day;
+			}
+			function initday14() {
+				day = 14;
+				return day;
+			}
+			function initday15() {
+				day = 15;
+				return day;
+			}
+			function initday16() {
+				day = 16;
+				return day;
+			}
+			function initday17() {
+				day = 17;
+				return day;
+			}
+			function initday18() {
+				day = 18;
+				return day;
+			}
+			function initday19() {
+				day = 19;
+				return day;
+			}
+			function initday20() {
+				day = 20;
+				return day;
+			}
+			function initday21() {
+				day = 21;
+				return day;
+			}
+			function initday22() {
+				day = 22;
+				return day;
+			}
+			function initday23() {
+				day = 23;
+				return day;
+			}
+			function initday24() {
+				day = 24;
+				return day;
+			}
+			function initday25() {
+				day = 25;
+				return day;
+			}
+			function initday26() {
+				day = 26;
+				return day;
+			}
+			function initday27() {
+				day = 27;
+				return day;
+			}function initday28() {
+				day = 28;
+				return day;
+			}
+			function initday29() {
+				day = 29;
+				return day;
+			}function initday30() {
+				day = 30;
+				return day;
+			}function initday31() {
+				day = 31;
+				return day;
+			}
